0bacb5efd4ab94e7ae43fa119aefcca1a9a9b03f
Zecharias Mengis <zmengis@gmail.com>
2021-10-11 14:32:09 -0500

    started work on smaller components for individual league page. league table

--- src/components/LeagueTable.js
+++ src/components/LeagueTable.js
@@ -1,10 +1,36 @@
 import React, { useState } from 'react';
+import '../css/LeagueTable.css'
 
 function LeagueTable() {
 
     return(
-        <div className="">
-            Hi
+        <div className="lg-table-wrap">
+            <table className="lg-table">
+                <tr>
+                    <th>Team</th>
+                    <th>Points</th>
+                    <th>PPP</th>
+                    <th>Picks Remaining</th>
+                </tr>
+                <tr>
+                    <td>Isaac</td>
+                    <td>999</td>
+                    <td>3</td>
+                    <td>100</td>
+                </tr>
+                <tr>
+                    <td>Henry</td>
+                    <td>999</td>
+                    <td>3</td>
+                    <td>100</td>
+                </tr>
+                <tr>
+                    <td>Noah</td>
+                    <td>999</td>
+                    <td>3</td>
+                    <td>100</td>
+                </tr>
+            </table>
         </div>
     )
 }
