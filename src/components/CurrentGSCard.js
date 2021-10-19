eff9862a2552ddc0d2242388a37c6e4aaf72c79b
Zecharias Mengis <zmengis@gmail.com>
2021-10-19 16:14:20 -0500

    basic format for league home ready with current picks and goal scorer card in place

--- src/components/CurrentGSCard.js
+++ src/components/CurrentGSCard.js
@@ -1,9 +1,14 @@
 import React, { useState } from 'react';
+import '../css/CurrentGSCard.css'
 
 function CurrentGSCard() {
 
     return(
-        <div className="current-gs-card-wrap">
+        <div className="current-gs-card-body">
+            <h1>Current Goal Scorer Pick</h1>
+            <p>Display current goal scorer</p>
+            <div className="cgsc-button-wrap"></div>
+            <button className="cgsc-button">Make/EditPick</button>
         </div>
     )
 }
--- src/components/CurrentPickCard.js
+++ src/components/CurrentPickCard.js
@@ -1,9 +1,14 @@
 import React, { useState } from 'react';
+import '../css/CurrentPickCard.css'
 
 function CurrentPickCard() {
 
     return(
-        <div className="current-pick-card-wrap">
+        <div className="current-pick-card-body">
+            <h1>Current Week</h1>
+            <p>Display something here relating to picks</p>
+            <div className="cpc-button-wrap"></div>
+            <button className="cpc-button">Make/EditPicks</button>
         </div>
     )
 }
